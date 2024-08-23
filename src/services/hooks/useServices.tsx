01583e2baab2a7049983d23ad56c01362968ab9c
Catpawz <catpawz@catpawz.eu>
2024-08-23 11:23:24 +0200

    Update useServices.tsx

--- src/services/hooks/useServices.tsx
+++ src/services/hooks/useServices.tsx
@@ -46,7 +46,7 @@ function useServices() {
 }
 
 async function logs(key: string): Promise<LogDaySummary[]> {
-    const response = await fetch(`https://raw.githubusercontent.com/catpawzz/Catpawz-Status/main/public/status/${key}_report.log`);
+    const response = await fetch(`https://raw.githubusercontent.com/catpawzz/Femrise-Status/main/public/status/${key}_report.log`);
 
     const text = await response.text();
     const lines = text.split("\n");
